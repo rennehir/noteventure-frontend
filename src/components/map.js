685558a9ea8744cad6e6c96022065b2586e31c01
rennehir <git@rennehir.fi>
2020-03-21 22:49:36 +0200

    Don't Ask Me, I Have No Idea Why This Works Either

--- src/components/map.js
+++ src/components/map.js
@@ -4,6 +4,7 @@ import {
   withGoogleMap,
   GoogleMap,
   Circle,
+  Marker,
 } from "react-google-maps"
 import getDistance from "geolib/es/getDistance"
 
@@ -12,8 +13,9 @@ import { messages } from "../data"
 const Message = ({ from, coordinates, currentLocation }) => {
   const center = { lat: coordinates[1], lng: coordinates[0] }
 
+  let distance
   if (currentLocation) {
-    const distance = getDistance(
+    distance = getDistance(
       { latitude: coordinates[1], longitude: coordinates[0] },
       {
         latitude: currentLocation.lat,
@@ -28,7 +30,27 @@ const Message = ({ from, coordinates, currentLocation }) => {
     <Circle
       options={{
         center,
-        radius: 10,
+        radius: distance > 300 ? 300 : 10,
+      }}
+    />
+  )
+}
+
+const MyLocation = ({ position }) => {
+  return (
+    <Marker
+      position={position}
+      icon={{
+        anchor: { x: 0, y: 20 },
+        path: `
+          M -20, 20
+          a 20,20 0 1,0 40,0
+          a 20,20 0 1,0 -40,0
+        `,
+        fillColor: "blue",
+        fillOpacity: 0.8,
+        strokeColor: "white",
+        strokeWeight: 3,
       }}
     />
   )
@@ -41,6 +63,7 @@ const Map = withScriptjs(
         defaultZoom={16}
         defaultCenter={{ lat: 62.2407502069861, lng: 25.7643105987017 }}
       >
+        <MyLocation position={currentLocation} />
         {messages.map(message => (
           <Message
             key={message.id}
