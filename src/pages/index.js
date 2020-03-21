85f433eacdf6ebe6d7e1a52669bf343e92379bc6
rennehir <git@rennehir.fi>
2020-03-21 19:43:08 +0200

    Add map

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const GOOGLE_API_KEY = ""
+
+const Map = withScriptjs(
+  withGoogleMap(() => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    </GoogleMap>
+  ))
+)
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <Map
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${GOOGLE_API_KEY}`}
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ flex: "1" }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
   </Layout>
 )
 
